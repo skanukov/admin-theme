c2f6e5fa8f58b714e2c753b37fdfe8421334da62
Sergey Kanukov <s.kanukov@gmail.com>
2016-07-12 10:10:06 +0300

    Add overlay to close active sidebar on click

--- app/assets/javascripts/admin/sidebar.js
+++ app/assets/javascripts/admin/sidebar.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function () {
-  $('[data-toggle="sidebar"]').click(function () {
+  $('[data-toggle="sidebar"], .wrapper__overlay').click(function () {
     $('.wrapper').toggleClass('wrapper--sidebar-active');
   });
 });
