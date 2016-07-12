f921b45ecac5b430b586e8d6d25959f02a83319a
Sergey Kanukov <s.kanukov@gmail.com>
2016-07-12 17:08:20 +0300

    Add detection of active menu link and use overflow for aside

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,4 +1,11 @@
 module Admin::ApplicationHelper
+  # Returns the class for specified menu item path
+  def class_for_menu_link(path)
+    css_class = 'sidebar__menu-link'
+    css_class += ' sidebar__menu-link--active' if current_page? path
+    css_class
+  end
+
   # Returns the actionbar icon for specified path
   def actionbar_icon_for_path(path)
     icon_name = if path.include? 'admin'
